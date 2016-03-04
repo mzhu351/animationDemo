57742e0c7a1e6259645a5ea91098f06f1e793e3f
Min Zhu <mzhu@financialengines.com>
2016-03-03 22:23:46 -0800

    partially function

--- app/Components/Anm9.js
+++ app/Components/Anm9.js
@@ -2,30 +2,69 @@ import React, {
   Component,
   Text,
   View,
-  StyleSheet
-} from 'react-native';
+  StyleSheet,
+  Animated
+  } from 'react-native';
+import Button from 'react-native-button';
+
+export default class Anm9 extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: true,
+      fadeAnim: new Animated.Value(0),
+    };
+    this.toggleAnm = this.toggleAnm.bind(this);
+  }
+  toggleAnm() {
+    console.log('called');
+    Animated.timing(
+      this.state.fadeAnim,
+      {toValue: 1 , duration: 1500},
+    ).start();
+    this.setState((state) => (
+      {show: !state.show}
+    ));
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button onPress={() => {this.toggleAnm();}}
+                style={styles.btn}>
+          Press {this.state.show ?
+                  'Show' : 'Hide'}
+        </Button>
+
+        <Animated.View style={[styles.box,{opacity: this.state.fadeAnim}]}>
+          <Text>Fadding {this.state.show ?
+                  'Out' : 'In'} </Text>
+        </Animated.View>
+
+      </View>
+    );
+  }
+};
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 65,
-    flex: 1
+    flex: 1,
+    paddingTop: 100,
   },
-  text: {
-    fontSize: 25,
-    color: '#333',
-    alignSelf: 'center'
+  btn: {
+    margin: 10,
+    backgroundColor: "#1fa0c8",
+    color: "white",
+    padding: 20
+  },
+  box: {
+    backgroundColor: 'deepskyblue',
+    borderWidth: 1,
+    borderColor: 'dodgerblue',
+    padding: 20,
+    margin: 20,
+    borderRadius: 10,
+    alignItems: 'center',
   }
+
 });
-
-export default class Anm9 extends Component{
-
-  render(){
-    return (
-      <View style={styles.container}>
-        <Text style={styles.text}>
-          Anm9
-        </Text>
-      </View>
-    )
-  }
-};
